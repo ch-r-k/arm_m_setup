076d4cddeedce826fa8ce983748228e5d35c7446
ch-r-k <ch-r-k>
2024-03-06 11:30:57 +0100

    update arm setup

---
+++ setup.sh
@@ -0,0 +1,17 @@
+set -e
+CONFIG_DIR=$(dirname "0")
+
+TARGET=$PWD
+
+if ["$CONFIG_DIR" = "."]; then
+    echo "This script must be called from the floder where the links shall be created in" > &2
+    exit 1
+fi
+
+function link ()
+{
+    ln -s "$CONFIG_DIR/$1" "$TARGET/$2"
+}
+
+link vscode .vscode
+link devcontainer .devcontainer
